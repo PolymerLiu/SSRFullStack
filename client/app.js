b05b9aefce4c119234848d4eb1712a735fedf96b
JeffreyLiuBit <17771770679@163.com>
2019-02-24 17:40:42 +0800

    feat:  服务端渲染的基本配置

--- client/app.js
+++ client/app.js
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App.jsx'
 
-ReactDOM.render(<App/>,document.body)
+ReactDOM.render(<App/>,document.getElementById('root'))
 
