3bc9580b4f56083443fc9e8b37fd1f5dba39af3f
JeffreyLiuBit <17771770679@163.com>
2019-03-02 19:19:55 +0800

    配置hot-module

--- client/app.js
+++ client/app.js
@@ -4,3 +4,10 @@ import App from './App.jsx'
 
 ReactDOM.render(<App/>,document.getElementById('root'))
 
+
+if (module.hot) {
+    module.hot.accept('./App.jsx',() => {
+        const NextApp = require('./App.jsx').default
+        ReactDOM.render(<NextApp/>,document.getElementById('root'))
+    })
+}
