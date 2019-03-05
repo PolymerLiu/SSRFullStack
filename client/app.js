36a59d1737bcd2384134016e2bb4c91b07cb15c2
JeffreyLiuBit <17771770679@163.com>
2019-03-05 23:31:37 +0800

    项目正式开始的初始化工作

--- client/app.js
+++ client/app.js
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from './App.jsx'
+import App from './views/App.jsx'
 
 ReactDOM.render(<App/>,document.getElementById('root'))
 
 
 if (module.hot) {
     module.hot.accept('./App.jsx',() => {
-        const NextApp = require('./App.jsx').default
+        const NextApp = require('./views/App.jsx').default
         ReactDOM.render(<NextApp/>,document.getElementById('root'))
     })
 }
---
+++ client/views/topic-list/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class TopicList extends React.Component {
+    render() {
+        return(
+            <div>this is topicList</div>
+        )
+    }
+}
