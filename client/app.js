c3bb98d0ca07f409321a420950c057f99fc40bb0
JeffreyLiuBit <17771770679@163.com>
2019-03-11 00:57:27 +0800

    修复export default   import BUG

--- client/app.js
+++ client/app.js
@@ -1,23 +1,32 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import App from './views/App.jsx'
-import BrowserRouter from 'react-router-dom'
+import App from './views/App'
+import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'mobx-react'
+import {AppContainer} from 'react-hot-loader'
 
 import appState from './store/app.state'
 const root = document.getElementById('root')
 
 
+const render = (Component) => {
+    ReactDOM.hydrate(
+        <AppContainer>
+            <Provider appState={appState}>
+                <BrowserRouter >
+                    <Component/>
+                </BrowserRouter>
+            </Provider>
+        </AppContainer>,
+        root
+    )
+}
+
+render(App)
+
 if (module.hot) {
     module.hot.accept('./views/App.jsx',() => {
         const NextApp = require('./views/App.jsx').default
-        ReactDOM.render(
-            <Provider appState={appState}>
-                <BrowserRouter >
-                    <NextApp/>
-                </BrowserRouter>
-            </Provider>,
-            root
-            )
+        render(NextApp)
     })
 }
